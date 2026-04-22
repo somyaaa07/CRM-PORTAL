58fb24ca4c9377c2eda190ab98ed527144ff0ae6
somya singh <somya.21b0131037@abes.ac.in>
2026-04-22 15:29:33 +0530

    backend push

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { register, login } from '../controllers/authController.js';
-import { auth, adminOnly } from '../middleware/auth.js';
+// import { auth, adminOnly } from '../middleware/auth.js';
 
 const router = express.Router();
 
 // POST /api/auth/register → Sirf admin karega (protected)
-router.post('/register', auth, adminOnly, register);
+router.post('/register', register);
 
 // POST /api/auth/login → Sabke liye open
 router.post('/login', login);
